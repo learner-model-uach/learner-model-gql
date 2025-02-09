ed8d0880262f39add69bb0a5d265d6087bcc74cb
Pablo Sáez <pablosaez1995@gmail.com>
2025-02-08 22:13:17 -0500

    try stuff

--- prepare.mjs
+++ prepare.mjs
@@ -1,15 +1,15 @@
-import { execSync } from "child_process";
+// import { execSync } from "child_process";
 
-if (process.env.CI) process.exit(0);
+// if (process.env.CI) process.exit(0);
 
-execSync("pnpm husky install", {
-  stdio: "inherit",
-});
+// execSync("pnpm husky install", {
+//   stdio: "inherit",
+// });
 
-execSync("pnpm -r db:generate", {
-  stdio: "inherit",
-});
+// execSync("pnpm -r db:generate", {
+//   stdio: "inherit",
+// });
 
-try {
-  execSync("pnpm -r db:link-env", {});
-} catch {}
+// try {
+//   execSync("pnpm -r db:link-env", {});
+// } catch {}
